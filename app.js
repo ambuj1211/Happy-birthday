20019fc24c7eb11e3198d60c161fa5cd7c9dcd07
Brilliant Ambuj <76522659+ambuj1211@users.noreply.github.com>
2024-01-07 18:53:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','Negar ' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','Ambuj' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
